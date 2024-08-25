9c4f1a0726b3085f2a98201a6fb93921fa89c6aa
Teodor Grigor <open-source@teogor.dev>
2024-08-25 15:12:07 +0300

    Add no-op function nothing()

--- composeApp/src/commonMain/kotlin/com/programmersbox/sudoku/App.kt
+++ composeApp/src/commonMain/kotlin/com/programmersbox/sudoku/App.kt
@@ -55,9 +55,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 import kotlin.collections.forEach
 import kotlin.time.Duration.Companion.milliseconds
 
-fun nothing() {
-
-}
+fun nothing() = Unit
 
 @OptIn(ExperimentalSudoklifyApi::class)
 @Composable
